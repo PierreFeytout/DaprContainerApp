2baf4dc655f60af47b8f94929844493cb6ac9896
Pierre Feytout <pierre.feytout@outlook.com>
2023-02-28 23:49:01 +0100

    Init respository with sample console and aspnet apps

---
+++ DaprCounter/Program.cs
@@ -0,0 +1,25 @@
+﻿using Dapr.Client;
+
+namespace DaprCounter
+{
+    using Dapr.Client;
+    public class Program
+    {
+        const string storeName = "statestore";
+        const string key = "counter";
+
+        public static async Task Main(string[] args)
+        {
+            var daprClient = new DaprClientBuilder().Build();
+            var counter = await daprClient.GetStateAsync<int>(storeName, key);
+
+            while (true)
+            {
+                Console.WriteLine($"Counter = {counter++}");
+
+                await daprClient.SaveStateAsync(storeName, key, counter);
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
